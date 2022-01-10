6299b6b7ad6168f7a901c7804f32dfc6e041734d
Micah Kim <85715752+micah-kim@users.noreply.github.com>
2022-01-10 22:53:00 +0900

    Add files via upload
    - added responsive design so the website works efficiently on mobile 
    - also fixed JS "map__link"

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
-const navLogo = documment.querySelector('#navbar__logo')
 
 // Display Mobile Menu
 const mobileMenu = () => {
@@ -19,6 +18,7 @@ var text = "I'm from ";
 let result = text.fontcolor("white");
 const period = "."
 let result2 = period.fontcolor("white");
+
 function changeWord() {
     map__list.innerHTML = result + "{ " + mapList[i] + " }" + result2;
 
@@ -32,47 +32,3 @@ function changeWord() {
 }
 
 window.onload = changeWord;
-
-// Show active menu when scrolling
-const highLightMenu = () => {
-    const elem = document.querySelector('.highlight');
-    const aboutMenu = document.querySelector('#about-page');
-    const skillsMenu = document.querySelector('#skills-page');
-    const experiencesMenu = document.querySelector('#experiences-page');
-    const projectsMenu = document.querySelector('#projects-page');
-    const contactsMenu = document.querySelector('#contacts-page');
-    let scrollPos = window.scrollY;
-
-    // adds 'highlight class to my menu items
-    if (window.innerWidth > 960 && scrollPos < 600) {
-        aboutMenu.classList.add('highlight');
-        skillsMenu.classList.remove('highlight');
-        return 
-    } else if (window.innerWidth > 960 && scrollPos < 1400) {
-        skillsMenu.classList.add('highlight');
-        aboutMenu.classList.remove('highlight');
-        experiencesMenu.classList.remove('highlight');
-        return
-    } else if (window.innerWidth > 960 && scrollPos < 2500) {
-        experiencesMenu.classList.add('highlight');
-        skillsMenu.classList.remove('highlight');
-        projectsMenu.classList.remove('highlight');
-        return
-    } else if (window.innerWidth > 960 && scrollPos < 3000) {
-        projectsMenu.classList.add('highlight');
-        experiencesMenu.classList.remove('highlight');
-        contactsMenu.classList.remove('highlight');
-        return
-    } else if (window.innerWidth > 960 && scrollPos < 3200) {
-        contactsMenu.classList.add('highlight');
-        projectsMenu.classList.remove('highlight');
-        return
-    }
-}
-
-if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
-    elem.classList.remove('highlight');
-}
-
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
