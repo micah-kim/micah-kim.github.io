ab3d4f855f0b7845baf092096033d8ba0edeea32
Micah Kim <85715752+micah-kim@users.noreply.github.com>
2022-01-12 00:47:10 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -32,3 +32,22 @@ function changeWord() {
 }
 
 window.onload = changeWord;
+
+var bttn = document.getElementById("back-to-top");
+var body = document.body;
+var docElem = document.documentElement;
+var offset = 100;
+var scrollPos;
+var docHeight;
+
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
+if (docHeight != 'undefined') {
+    offset = 1;
+}
+
+// Add scroll event listener
+window.addEventListener("scroll", function(event) {
+    scrollPos = body.scrollTop || docElem.scrollTop;
+
+    bttn.className = (scrollPos > offset) ? "visible" : "";
+});
